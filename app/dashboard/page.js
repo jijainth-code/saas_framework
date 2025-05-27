b5fa6d95ab4da5d9d8490e87074a30bf0b871629
jijainth <jijainth.dhinakaran@vestigas.com>
2025-05-27 17:56:33 +0200

    added google authentication

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,3 +1,5 @@
+
+
 export default function dashboard() {
   return (
     <div>
--- auth.js
+++ auth.js
@@ -1,5 +1,10 @@
 import NextAuth from "next-auth"
- 
+import { MongoDBAdapter } from "@auth/mongodb-adapter"
+import clientPromise from "@/libs/mongo"
+import Google from "next-auth/providers/google"
+import Resend from "next-auth/providers/resend"
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
+  adapter: MongoDBAdapter(clientPromise),
+  providers: [Resend , Google],
 })
